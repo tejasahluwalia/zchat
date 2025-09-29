d0625b2dfcdb58630f6289f320e0b68110b6d0a9
Tejas Ahluwalia <tejas@tujux.com>
2025-09-29 07:47:24 +0530

    no idea

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -5,61 +5,6 @@
 
 declare module "sst" {
   export interface Resource {
-    "DeepseekApiKey": {
-      "type": "sst.sst.Secret"
-      "value": string
-    }
-    "GithubOauthClientID": {
-      "type": "sst.sst.Secret"
-      "value": string
-    }
-    "GithubOauthClientSecret": {
-      "type": "sst.sst.Secret"
-      "value": string
-    }
-    "ZchatAuth": {
-      "type": "sst.aws.Auth"
-      "url": string
-    }
-    "ZchatDB": {
-      "database": string
-      "host": string
-      "password": string
-      "port": number
-      "type": "sst.aws.Postgres"
-      "username": string
-    }
-    "ZchatReplicationBucket": {
-      "name": string
-      "type": "sst.aws.Bucket"
-    }
-    "ZchatReplicationManager": {
-      "service": string
-      "type": "sst.aws.Service"
-      "url": string
-    }
-    "ZchatViewSyncer": {
-      "service": string
-      "type": "sst.aws.Service"
-      "url": string
-    }
-    "ZchatVpc": {
-      "bastion": string
-      "type": "sst.aws.Vpc"
-    }
-    "ZchatWeb": {
-      "service": string
-      "type": "sst.aws.Service"
-      "url": string
-    }
-    "ZeroAuthSecret": {
-      "type": "sst.sst.Secret"
-      "value": string
-    }
-    "ZeroPermissionsDeployer": {
-      "name": string
-      "type": "sst.aws.Function"
-    }
   }
 }
 /// <reference path="sst-env.d.ts" />
